4e48600dc9672b28dca77bd6bc9a2fc26386f14f
Saif AlDeen Sameh <seifeldeansameh@cis.asu.edu.eg>
2022-01-18 16:42:56 +0200

    upload,search,export(api,fullstack) with path

--- uploading_data/url.py
+++ uploading_data/url.py
@@ -6,7 +6,11 @@ urlpatterns = [
     path('upload/done/',views.done_upload,name='upload_done'),
     path('search_sku/',views.search_skus,name='search_sku'),
     path('search_batch/',views.search_ticket_batch,name='search_batch'),
-    path('report/', views.report_search, name='report_search')
+    path('report/', views.report_search, name='report_search'),
+
+    path('upload_api', views.Upload_api.as_view(), name='index'),
+    path('search_sku_api/', views.search_skus_api, name='search_sku'),
+    path('search_batch_api/', views.search_ticket_batch_api, name='search_batch'),
 
     # path('signup',views.signup,name='signup'),
 ]
